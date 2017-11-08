d91e62a29b39a99c61660b7c4f64ea3606f00386
Anna Bair <annabair@mit.edu>
2017-11-07 22:41:41 -0500

    Fixed LR Code

--- LR.py
+++ LR.py
@@ -1,26 +1,44 @@
-from sklearn import linear_model as lm
-from sklearn import grid_search as gs
+from sklearn import linear_model 
+from sklearn.model_selection import GridSearchCV
 import numpy as np
 from read_data import *
 from math import *
 from plotBoundary import *
 
-#X_train, Y_train = get_traindata() 
-print X_train.shape
-print Y_train.shape
+X_train, y_train = get_traindata() 
+X_val, y_val = get_valdata()
+
+X_train_and_val = np.concatenate((X_train, X_val))
+y_train_and_val = np.concatenate((y_train, y_val))
+
+print(X_train.shape)
+print(y_train.shape)
 p = 'l2'
-LR=lm.LogisticRegression(penalty=p, C=1.0, max_iter=100, intercept_scaling=100, random_state=1)
-#param = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000] }
-#clf = gs.GridSearchCV(LR, param)
-LR.fit(X_train,Y_train.ravel())
+LR = linear_model.LogisticRegression()
 
-print len(LR.coef_.ravel())
+LR.fit(X_train,y_train)
 
-def predictLR(x):
-    print x
-    return LR.coef_.ravel() * mat(x).T + LR.intercept_
-
-plotDecisionBoundary(X_train, Y_train, predictLR, [0], title = 'LR Train')
-pl.show()
+print("LR Score: ", LR.score(X_val, y_val))
 
 
+params = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 
+          'penalty': ['l1', 'l2']
+         }
+clf = GridSearchCV(LR, params)
+clf.fit(X_train_and_val, y_train_and_val)
+
+print(clf.best_estimator_)
+print("Best score: ", clf.best_score_)
+print("Best C: ", clf.best_estimator_.C)
+print("Best penalty: ", clf.best_estimator_.penalty)
+
+
+'''
+This takes a while to run; here are the results I got:
+LR score: 0.694444444444
+Output of GridSearchCV:
+
+Best score:  0.826570048309
+Best C:  0.1
+Best penalty:  l1
+'''
