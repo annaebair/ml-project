752f74793f31124f6874262c73632a66cab007af
amirhassank <amirhassank@yahoo.com>
2017-12-08 19:05:54 -0500

    pca fixed

--- pca.py
+++ pca.py
@@ -1,15 +1,6 @@
 import numpy as np
 from sklearn.decomposition import PCA
-
-
-
-
-
-
-
-
-
-
+from read_data import *
 
 
 def apply_pca(X, num_components):
@@ -19,11 +10,15 @@ def apply_pca(X, num_components):
 
 	
 	pca = PCA(n_components=num_components)
-	X_transform = pca.transform(X)
+	X_transform = pca.fit_transform(X)
 
 	return X_transform
 
 
+X_train, Y_train = get_traindata() 
+X,Y = clean_data(.95,X_train,Y_train)
+
+print( apply_pca(X,10).shape )
 
 
 	
