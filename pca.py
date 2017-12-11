3c698ae3fe6736eaa18efaeb8db55918dc787b68
amirhassank <amirhassank@yahoo.com>
2017-12-10 20:28:54 -0500

    SparsePCA

--- pca.py
+++ pca.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn.decomposition import SparsePCA
 from read_data import *
 
 
@@ -14,11 +15,18 @@ def apply_pca(X, num_components):
 
 	return X_transform
 
+def apply_SparsePCA(X, num_components, alpha=1 ,ridge_alpha=0.1):
+    
+    sparse_pca = SparsePCA(n_components=num_components , alpha = alpha, ridge_alpha = ridge_alpha)
+    X_transform = sparse_pca.fit_transform(X)
+    
+    return X_transform
+
 
 X_train, Y_train = get_traindata() 
 X,Y = clean_data(.95,X_train,Y_train)
 
-print( apply_pca(X,10).shape )
+print( apply_SparsePCA(X,10).shape )
 
 
 	
